2c507fddb4049309cc2fadebc0af22b25f19b1da
jay <jay@grolar>
2025-01-19 15:06:04 +0100

    test: benchmark map vs list performance. Keep list

---
+++ ingredient/map_test.go
@@ -0,0 +1,48 @@
+package ingredient
+
+import (
+	"ingredient-calculator/ingredient/unit"
+	"testing"
+)
+
+type Map map[string]Element
+
+func (m Map) GetElement(ingrName string) (Element, error) {
+	return m[ingrName], nil
+}
+
+func BenchmarkGetElementFromMap(b *testing.B) {
+	m := Map{
+		"farine":    CreateElement("farine", 1000, unit.G),
+		"eau":       CreateElement("eau", 1000, unit.G),
+		"aze":       CreateElement("aze", 1000, unit.G),
+		"toto":      CreateElement("toto", 1000, unit.G),
+		"ajf":       CreateElement("ajf", 1000, unit.G),
+		"ojzepv":    CreateElement("ojzepv", 1000, unit.G),
+		"zporvnio":  CreateElement("zporvnio", 1000, unit.G),
+		"zoaeijvn":  CreateElement("zoaeijvn", 1000, unit.G),
+		"xcvmlkndf": CreateElement("xcvmlkndf", 1000, unit.G),
+	}
+
+	for i := 0; i < b.N; i++ {
+		m.GetElement("farine")
+	}
+}
+
+func BenchmarkGetElementFromList(b *testing.B) {
+	l := List{
+		CreateElement("farine", 1000, unit.G),
+		CreateElement("eau", 1000, unit.G),
+		CreateElement("aze", 1000, unit.G),
+		CreateElement("toto", 1000, unit.G),
+		CreateElement("ajf", 1000, unit.G),
+		CreateElement("ojzepv", 1000, unit.G),
+		CreateElement("zporvnio", 1000, unit.G),
+		CreateElement("zoaeijvn", 1000, unit.G),
+		CreateElement("xcvmlkndf", 1000, unit.G),
+	}
+
+	for i := 0; i < b.N; i++ {
+		l.GetElement("farine")
+	}
+}
--- todo.md
+++ todo.md
@@ -1,3 +1,1 @@
-- ingredient.List is not user friendly
-    * should be able to l["ingrName"].Qty to access content
 - implement ConvertList
